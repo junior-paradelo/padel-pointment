6afff6727f107b00aa7c317a3a529c16eb91254e
Junior Paradelo <juniorparadelo@gmail.com>
2025-04-06 19:33:18 +0200

    feat: add response-time & compression middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const responseTime = require("response-time");
+const compression = require("compression");
 const routes = require("./routes");
 
 const app = express();
@@ -8,6 +10,8 @@ const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(responseTime());
+app.use(compression());
 app.use(loggerMiddleware);
 app.use(errorHandler);
 app.use("/api", routes);
