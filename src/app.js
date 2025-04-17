00b2eace6812ecff0ceeb7e7a5d22d06531317c6
Junior Paradelo <juniorparadelo@gmail.com>
2025-04-17 17:33:30 +0200

    feat: add cors middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const responseTime = require("response-time");
 const compression = require("compression");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
 const loggerMiddleware = require("./middlewares/logger");
 const errorHandler = require("./middlewares/errorHandler");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(responseTime());
